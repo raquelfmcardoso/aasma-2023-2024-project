d5756a37e78c62a697b626d6cbe5f5f2e9c99915
raquel cardoso <raquel.cardoso@toonelada.pt>
2024-05-07 15:39:48 +0100

    Added initial files

---
+++ tiger_deer_pursuit/test/test.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('/home/raquel/aasma/tiger_deer_pursuit')  # Add the path to the 'tiger_deer_pursuit' package
+from tiger_deer_pursuit import tiger_deer_pursuit_v0
+
+env = tiger_deer_pursuit_v0.env(render_mode="human")
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if termination or truncation:
+        action = None
+    else:
+        # this is where you would insert your policy
+        action = env.action_space(agent).sample()
+
+    env.step(action)
+env.close()
---
+++ tiger_deer_pursuit/tiger_deer_pursuit_v0.py
@@ -0,0 +1,3 @@
+from tiger_deer_pursuit.env.tiger_deer_pursuit import ManualPolicy, env, parallel_env, raw_env
+
+__all__ = ["ManualPolicy", "env", "parallel_env", "raw_env"]
